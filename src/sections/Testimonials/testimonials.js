7374a37f1cf9c545703d51613e292f09fdd3e4a7
ratsy-dev <raghavnayakcs88@gmail.com>
2024-04-26 20:30:38 +0530

    changes done

--- src/sections/Testimonials/testimonials.js
+++ src/sections/Testimonials/testimonials.js
@@ -17,21 +17,21 @@ const Testimonials = () => {
  return (
   <section id="testimonials">
    <h2>What My Clients Say</h2>
-   <p>These are unbiased testmonials from some of my clients</p>
+   <p>These are unbiased testmonials from my client</p>
    <div className="container">
     <Swiper
-     slidesPerView={1}
-     spaceBetween={30}
-     breakpoints={{
-      601: { slidesPerView: 2 },
-      1025: { slidesPerView: 3 },
-     }}
-     pagination={{
-      clickable: true,
-     }}
-     modules={[Pagination, Autoplay]}
-     autoplay={true}
-     className="mySwiper"
+    //  slidesPerView={1}
+    //  spaceBetween={30}
+    //  breakpoints={{
+    //   601: { slidesPerView: 2 },
+    //   1025: { slidesPerView: 3 },
+    //  }}
+    //  pagination={{
+    //   clickable: true,
+    //  }}
+    //  modules={[Pagination, Autoplay]}
+    //  autoplay={true}
+    //  className="mySwiper"
     >
      {testimonials.map((testimonial) => (
       <SwiperSlide key={testimonial.id}>
