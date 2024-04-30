dd33263e289c35435336157b960ef266f7898d9b
ratsy-dev <raghavnayakcs88@gmail.com>
2024-04-30 10:33:27 +0530

    update

--- src/sections/portfolio/data.js
+++ src/sections/portfolio/data.js
@@ -2,21 +2,22 @@ import Image1 from "../../assets/project1.jpg";
 import Image2 from "../../assets/project2.jpg";
 import Image3 from "../../assets/project3.jpg";
 import Image4 from "../../assets/project4.jpg";
+import Image5 from "../../assets/project5.jpg";
 
 const data = [
  {
   id: 1,
-  category: "full stack project",
+  category: "front-end",
   image: Image1,
-  title: "POS Web Application",
+  title: "POS Website",
   desc:
-   "The Principality of Cogito (POS) webapp gives you the ability to register and run your company as a fully location independent company. This means that you can run your business from anywhere in the world, connect with a network of individuals and businesses in the Cogito Metaverse and enjoy the benefits of simple taxes, low bureaucracy, and simple international banking.!",
+   "The Principality of Cogito (POS) website gives you the ability to register and run your company as a fully location independent company. This means that you can run your business from anywhere in the world, connect with a network of individuals and businesses in the Cogito Metaverse and enjoy the benefits of simple taxes, low bureaucracy, and simple international banking.!",
   demo: "https://www.principalityofcogito.com/",
   github: "https://github.com/raghav-cogito",
  },
  {
   id: 2,
-  category: "full stack project",
+  category: "back-end",
   image: Image2,
   title: "Fintech Web Application",
   desc:
@@ -30,7 +31,7 @@ const data = [
   image: Image3,
   title: "POS Mobile Application",
   desc:
-   "The Principality of Cogito has created the Cogito Metaverse which is a revolutionary digital realm. The metaverse is often associated with the future of the Internet, known as Web 3.0, where ownership and control of information are distributed among the people who use it, rather than being centralised in the hands of a few platforms like Google or Facebook. The vision for the Cogito Metaverse is to create a virtual space where people can connect with each other socially, engage in business activities, communicate, and learn. It aims to break traditional borders and be accessible to citizens from all around the world, fostering a global community.",
+   "The Principality of Cogito (POS) website gives you the ability to register and run your company as a fully location independent company. This means that you can run your business from anywhere in the world, connect with a network of individuals and businesses in the Cogito Metaverse and enjoy the benefits of simple taxes, low bureaucracy, and simple international banking.!.",
   demo: "https://play.google.com/store/apps/details?id=com.cogitoapp",
   github: "https://github.com/raghav-cogito",
  },
@@ -41,6 +42,17 @@ const data = [
   title: "Cogito Metaverse Mobile App",
   desc:
    "The Principality of Cogito has created the Cogito Metaverse which is a revolutionary digital realm. The metaverse is often associated with the future of the Internet, known as Web 3.0, where ownership and control of information are distributed among the people who use it, rather than being centralised in the hands of a few platforms like Google or Facebook. The vision for the Cogito Metaverse is to create a virtual space where people can connect with each other socially, engage in business activities, communicate, and learn. It aims to break traditional borders and be accessible to citizens from all around the world, fostering a global community",
+  demo:
+   "https://play.google.com/store/search?q=cogito%20metaverse&c=apps&hl=en_IN&gl=US",
+  github: "https://github.com/raghav-cogito",
+ },
+ {
+  id: 5,
+  category: "front-end",
+  image: Image5,
+  title: "Cogito Metaverse Website",
+  desc:
+   "The Cogito Metaverse is an advanced digital space where users can engage and interact in a virtual world. It brings together intelligent individuals who aim to grow investments, build businesses, and enhance the lives of ordinary people. Within the Cogito Metaverse, members have the opportunity to support each other, engage in free trade and finance, and benefit from financial education. The platform offers avenues for investment and wealth-building that differ from traditional banks and investment companies. Users can establish companies and asset protection trusts within the Cogito Metaverse to safeguard their wealth and explore innovative paths to prosperity.",
   demo:
    "https://play.google.com/store/search?q=cogito%20metaverse&c=apps&hl=en_IN&gl=US",
   github: "https://github.com/raghav-cogito",
