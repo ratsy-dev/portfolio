5aa5e80503933c47bab213de7ef130233f8eeed3
ratsy-dev <raghavnayakcs88@gmail.com>
2025-07-21 17:09:22 +0530

    changes in footer and sections

--- src/sections/about/about.js
+++ src/sections/about/about.js
@@ -5,42 +5,42 @@ import Card from "../../components/card";
 import "./about.css";
 
 const About = () => {
- return (
-  <section id="about" data-aos="fade-in">
-   <div className="container about__container">
-    <div className="about__left">
-     <div className="about__portrait">
-      <img src={AboutImage} alt="About Image" />
-     </div>
-    </div>
-    <div className="about__right">
-     <h2>About Me</h2>
-     <div className="about__cards">
-      {data.map((item) => (
-       <Card key={item.id} className="about__card">
-        <span className="about__card-icon">{item.icon}</span>
-        <h5>{item.title}</h5>
-        <small>{item.desc}</small>
-       </Card>
-      ))}
-     </div>
-     <p>
-      Building projects has always been my passion. Being in the web development
-      industry for over 3 years and working with my client in New Zealand, I'm
-      always motivated to do more!
-     </p>
-     <p>
-      Hi, my name is Raghav Nayak from Mumbai, Maharashtra. I'm a full-stack web
-      developer with a Bachelors degree in Computer Applications (BCA). My top
-      priority is to get your business online the right way, giving you
-      industry-standard design and all the functionality you need to operate
-      smoothly online. Get in touch today with the details of your project let's
-      get started!
-     </p>
-    </div>
-   </div>
-  </section>
- );
+  return (
+    <section id="about" data-aos="fade-in">
+      <div className="container about__container">
+        <div className="about__left">
+          <div className="about__portrait">
+            <img src={AboutImage} alt="About Image" />
+          </div>
+        </div>
+        <div className="about__right">
+          <h2>About Me</h2>
+          <div className="about__cards">
+            {data.map((item) => (
+              <Card key={item.id} className="about__card">
+                <span className="about__card-icon">{item.icon}</span>
+                <h5>{item.title}</h5>
+                <small>{item.desc}</small>
+              </Card>
+            ))}
+          </div>
+          <p>
+            Building projects has always been my passion. Being in the web
+            development industry for over 3 years and working with my client in
+            New Zealand, I'm always motivated to do more!
+          </p>
+          <p>
+            Hi, my name is Raghav Nayak from Mumbai, Maharashtra. I'm a
+            full-stack web developer currently pursuing Bachelors degree in
+            Computer Applications (BCA). My top priority is to get your business
+            online the right way, giving you industry-standard design and all
+            the functionality you need to operate smoothly online. Get in touch
+            today with the details of your project let's get started!
+          </p>
+        </div>
+      </div>
+    </section>
+  );
 };
 
 export default About;
--- src/sections/footer/footer.js
+++ src/sections/footer/footer.js
@@ -2,34 +2,34 @@ import { links, socials } from "./data";
 import "./footer.css";
 
 const Footer = () => {
- return (
-  <footer>
-   <div className="container footer__container">
-    <ul className="nav__menu">
-     {links.map((fLink) => (
-      <li key={fLink.id}>
-       <a href={fLink.link}>{fLink.title}</a>
-      </li>
-     ))}
-    </ul>
-    <div className="footer__socials">
-     {socials.map((social) => (
-      <a
-       key={social.id}
-       href={social.link}
-       target="_blank"
-       rel="noopener noreferrer"
-      >
-       {social.icon}
-      </a>
-     ))}
-    </div>
-   </div>
-   <div className="footer__copyright">
-    <small>2024 Raghav's Portfolio &copy; All Rights Reserved</small>
-   </div>
-  </footer>
- );
+  return (
+    <footer>
+      <div className="container footer__container">
+        <ul className="nav__menu">
+          {links.map((fLink) => (
+            <li key={fLink.id}>
+              <a href={fLink.link}>{fLink.title}</a>
+            </li>
+          ))}
+        </ul>
+        <div className="footer__socials">
+          {socials.map((social) => (
+            <a
+              key={social.id}
+              href={social.link}
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              {social.icon}
+            </a>
+          ))}
+        </div>
+      </div>
+      <div className="footer__copyright">
+        <small>2025 Raghav's Portfolio &copy; All Rights Reserved</small>
+      </div>
+    </footer>
+  );
 };
 
 export default Footer;
