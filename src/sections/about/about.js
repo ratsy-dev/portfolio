c72075ae705015883cf370d242b90530bc400c83
ratsy-dev <raghavnayakcs88@gmail.com>
2025-07-21 21:51:09 +0530

    changes done

--- src/sections/about/about.js
+++ src/sections/about/about.js
@@ -1,5 +1,4 @@
 import AboutImage from "../../assets/about.jpg";
-import { HiDownload } from "react-icons/hi";
 import data from "./data";
 import Card from "../../components/card";
 import "./about.css";
@@ -10,7 +9,7 @@ const About = () => {
       <div className="container about__container">
         <div className="about__left">
           <div className="about__portrait">
-            <img src={AboutImage} alt="About Image" />
+            <img src={AboutImage} />
           </div>
         </div>
         <div className="about__right">
--- src/sections/header/data.js
+++ src/sections/header/data.js
@@ -1,16 +1,15 @@
 import { AiOutlineInstagram } from "react-icons/ai";
 import { AiOutlineTwitter } from "react-icons/ai";
-import { AiOutlineDribbble } from "react-icons/ai";
 import { AiFillGithub } from "react-icons/ai";
 
 const data = [
- {
-  id: 1,
-  link: "https://www.instagram.com/ratsy_dev/",
-  icon: <AiOutlineInstagram />,
- },
- { id: 2, link: "https://twitter.com/ratsycs88", icon: <AiOutlineTwitter /> },
- { id: 3, link: "https://github.com/raghav-cogito", icon: <AiFillGithub /> },
+  {
+    id: 1,
+    link: "https://www.instagram.com/ratsy_dev/",
+    icon: <AiOutlineInstagram />,
+  },
+  { id: 2, link: "https://twitter.com/ratsycs88", icon: <AiOutlineTwitter /> },
+  { id: 3, link: "https://github.com/raghav-cogito", icon: <AiFillGithub /> },
 ];
 
 export default data;
