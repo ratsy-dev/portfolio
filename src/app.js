cdff01283461e8afb6561209a5f81692a6f8672a
ratsy-dev <raghavnayakcs88@gmail.com>
2025-07-21 17:28:07 +0530

    floating navbar fixed

--- src/app.js
+++ src/app.js
@@ -10,63 +10,77 @@ import Footer from "./sections/footer/footer";
 import FloatingNav from "./sections/floating-nav/floatingnav";
 import Theme from "./theme/theme";
 import { useThemeContext } from "./context/theme-context";
-import { useRef, useState, useEffect } from "react";
+import { useRef, useState, useEffect, useCallback } from "react";
 
 const App = () => {
- const { themeState } = useThemeContext();
+  const { themeState } = useThemeContext();
 
- const mainRef = useRef();
- const [showFloatingNav, setShowFloatingNav] = useState(true);
- const [siteYPostion, setSiteYPosition] = useState(0);
+  const mainRef = useRef();
+  const [showFloatingNav, setShowFloatingNav] = useState(true);
+  const [siteYPostion, setSiteYPosition] = useState(0);
 
- const showFloatingNavHandler = () => {
-  setShowFloatingNav(true);
- };
+  const showFloatingNavHandler = () => {
+    setShowFloatingNav(true);
+  };
 
- const hideFloatingNavHandler = () => {
-  setShowFloatingNav(false);
- };
+  const hideFloatingNavHandler = () => {
+    setShowFloatingNav(false);
+  };
 
- // check if floating nav should be shown or hidden
- const floatingNavToggleHandler = () => {
-  // check if we scrolled up or down at least 20px
-  if (
-   siteYPostion < mainRef?.current?.getBoundingClientRect().y - 20 ||
-   siteYPostion > mainRef?.current?.getBoundingClientRect().y + 20
-  ) {
-   showFloatingNavHandler();
-  } else {
-   hideFloatingNavHandler();
-  }
+  // check if floating nav should be shown or hidden
+  // const floatingNavToggleHandler = () => {
+  //   // check if we scrolled up or down at least 20px
+  //   if (
+  //     siteYPostion < mainRef?.current?.getBoundingClientRect().y - 20 ||
+  //     siteYPostion > mainRef?.current?.getBoundingClientRect().y + 20
+  //   ) {
+  //     showFloatingNavHandler();
+  //   } else {
+  //     hideFloatingNavHandler();
+  //   }
 
-  setSiteYPosition(mainRef?.current?.getBoundingClientRect().y);
- };
+  //   setSiteYPosition(mainRef?.current?.getBoundingClientRect().y);
+  // };
 
- useEffect(() => {
-  const checkYPosition = setInterval(floatingNavToggleHandler, 2000);
+  // check if floating nav should be shown or hidden
+  const floatingNavToggleHandler = useCallback(() => {
+    // check if we scrolled up or down at least 20px
+    const currentY = mainRef?.current?.getBoundingClientRect().y;
 
-  // cleanup function
-  return () => clearInterval(checkYPosition);
- }, [siteYPostion]);
+    if (siteYPostion < currentY - 20 || siteYPostion > currentY + 20) {
+      showFloatingNavHandler();
+    } else {
+      hideFloatingNavHandler();
+    }
 
- return (
-  <main
-   className={`${themeState.primary} ${themeState.background}`}
-   ref={mainRef}
-  >
-   <Navbar />
-   <Header />
-   <About />
-   <Services />
-   <Portfolio />
-   <Testimonials />
-   <FAQs />
-   <Contact />
-   <Footer />
-   <Theme />
-   {showFloatingNav && <FloatingNav />}
-  </main>
- );
+    setSiteYPosition(currentY);
+  }, [siteYPostion]);
+
+  useEffect(() => {
+    const checkYPosition = setInterval(floatingNavToggleHandler, 2000);
+
+    // cleanup function
+    return () => clearInterval(checkYPosition);
+  }, [floatingNavToggleHandler]);
+
+  return (
+    <main
+      className={`${themeState.primary} ${themeState.background}`}
+      ref={mainRef}
+    >
+      <Navbar />
+      <Header />
+      <About />
+      <Services />
+      <Portfolio />
+      <Testimonials />
+      <FAQs />
+      <Contact />
+      <Footer />
+      <Theme />
+      {showFloatingNav && <FloatingNav />}
+    </main>
+  );
 };
 
 export default App;
